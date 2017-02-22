81fc80861c0256e746ce36723c49cfdbd0d1aa7a
mdsecurity <mattiasduffy@MacBook-Woah.local>
2017-02-22 11:30:50 -0500

    WE HAVE A SERVICE WORKER NOW it doesnt do much yet but be patient ;)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -32,7 +32,7 @@ import { ShowHideArrowComponent } from './context-bar/show-hide-arrow/show-hide-
 	],
 	imports: [
 		BrowserModule,
-		FormsModule,
+		// FormsModule,
 		HttpModule
 	],
 	providers: [],
--- src/app/place-window/area/area.component.ts
+++ src/app/place-window/area/area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { DataProviderService } from '../../data-provider.service';
 import { Place } from '../../Place';
 @Component({
