b3f1a2c0fd69e6100c5aa3f105c3f2a794d392c4
Mattias <mduffy8@gmu.edu>
2017-09-23 16:52:52 -0400

    small changes to search

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -2,19 +2,20 @@ import React from 'react'
 import {withStyles} from 'material-ui/styles';
 import FacilityCard from '../containers/FacilityCard'
 import Grid from 'material-ui/Grid';
+import fuzzysearch from 'fuzzysearch';
 
 const CardContainer = ({classes, searchTerm,facilities}) => {
     const filterCards = (facility) => {
-        return facility.name.includes(searchTerm)
+        const name = facility.facility_name.toLowerCase()
+        return name.includes(searchTerm.toLowerCase())
     }
-    console.log(searchTerm)
     return (
         <Grid container className={classes.root} spacing={24}>
-            {facilities.map(item =>
-                <Grid key={item.slug} item>
+            {facilities.filter(filterCards).map(item =>{
+                return(<Grid key={item.slug} item>
                     <FacilityCard facility={item}/>
-                </Grid>
-            )}
+                </Grid>)
+            })}
         </Grid>
     )
 }
