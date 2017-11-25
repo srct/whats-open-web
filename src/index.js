c5043f692d3b531d68ddc6709a316085f601e177
Mattias <mduffy8@gmu.edu>
2017-11-24 20:20:45 -0500

    fixed scss not updating

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import ReduxThunk from 'redux-thunk';
 import reducers from './reducers/index';
 import {MuiThemeProvider} from 'material-ui/styles';
 import theme from './theme';
-import './styles/whatsOpen.css';
+import './styles/build/whatsOpen.css';
 
 // Create a history of your choosing (we're using a browser history in this case)
 const history = createHistory();
