81623eb5552f44040c1125f393045ab85f015e8b
Mattias <mduffy8@gmu.edu>
2018-01-14 12:48:47 -0500

    fix to ios search bar issue

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -93,7 +93,7 @@ class SearchBar extends React.Component {
                 <Input
                     placeholder="Name, Location, etc."
                     disableUnderline
-                    className={'search-bar-input'}
+                    className={classNames('search-bar-input-open' && !this.state.isMobileOpen,'hide-search-input' && this.state.isMobileOpen)}
                     onChange={this.handleChange}
                     onFocus={this.handleFocus}
                     onBlur={this.handleBlur}
